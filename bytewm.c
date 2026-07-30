c853cb27b2b05d4b46fc576869bf6bdeb819c2b2
d2tx <d2tx@users.noreply.github.com>
2026-07-30 02:16:13 +0000

    feat: resize scratchpad to 22%x20% and add border

--- bytewm.c
+++ bytewm.c
@@ -1689,11 +1689,11 @@ manage(Window w, XWindowAttributes *wa)
 			scratchpad = c;
 			scratchpad->tags = 0;
 			scratchpad->isfloating = 1;
-			scratchpad->bw = 0;
-			scratchpad->x = selmon->wx + selmon->ww / 4;
-			scratchpad->y = selmon->wy + selmon->wh * 0.1;
-			scratchpad->w = selmon->ww / 2;
-			scratchpad->h = selmon->wh * 0.5;
+			scratchpad->bw = 2;
+			scratchpad->x = selmon->wx + selmon->ww * 0.39;
+			scratchpad->y = selmon->wy + selmon->wh * 0.15;
+			scratchpad->w = selmon->ww * 0.22;
+			scratchpad->h = selmon->wh * 0.2;
 			resizeclient(c, scratchpad->x, scratchpad->y,
 			             scratchpad->w, scratchpad->h);
 		}
